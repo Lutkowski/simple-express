2f7783b8cc9918090b32922365f424f02874f720
Lutkowski <anton.lutkovskiy@gmail.com>
2024-06-23 20:49:35 +0300

    docs: add swagger docs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import router from "./routes.js";
 import dotenv from "dotenv"
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 
 dotenv.config()
 
@@ -8,6 +10,26 @@ const PORT = process.env.PORT | 3000
 
 const app = express()
 
+const swaggerConfig = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'simple express app API with Swagger',
+            version: '1.0.0',
+            description: 'Простое приложение на express',
+        },
+        servers: [
+            {
+                url: `http://localhost:${PORT}`
+            },
+        ],
+    },
+    apis: ['./src/routes.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerConfig)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.use('/static', express.static('public'))
 app.use('/', router)
 
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,27 @@ import {Router} from "express"
 
 const router = new Router()
 
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     responses:
+ *       200:
+ *         description: Возвращает сообщение Привет, Мир!.
+ */
 router.get('/', (req, res) => {
     res.status(200).send("Hello, World!")
 })
 
+/**
+ * @swagger
+ * /about:
+ *   get:
+ *     responses:
+ *       200:
+ *         description: Возвращает сообщение с информацией о приложении.
+ */
 router.get('/about', (req, res) => {
     res.status(200).send("Это простое приложение на Express.js")
 })
