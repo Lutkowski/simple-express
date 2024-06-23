a4c4cafc0a4ff7949092111942362a58d96403bb
Lutkowski <anton.lutkovskiy@gmail.com>
2024-06-23 17:21:53 +0300

    feat: add about route and non-existing routes handling

--- src/app.js
+++ src/app.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) => {
     res.status(200).json("Hello, World!")
 })
 
+app.get('/about', (req, res) => {
+    res.status(200).json("Это простое приложение на Express.js")
+})
+
+app.all('*', (req, res) => {
+    res.status(404).json("Страница не найдена")
+})
 app.listen(PORT, () => {
-    console.log(`The server started on port ${PORT}.`)
+    console.log(`The server started on http://localhost:${PORT}/.`)
 })
