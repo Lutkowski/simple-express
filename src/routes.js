1e9ae03cc7293ea934f047106f0df383e7187842
Lutkowski <anton.lutkovskiy@gmail.com>
2024-06-23 18:07:25 +0300

    refactor: switch to res.send for text response

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,15 @@ import {Router} from "express"
 const router = new Router()
 
 router.get('/', (req, res) => {
-    res.status(200).json("Hello, World!")
+    res.status(200).send("Hello, World!")
 })
 
 router.get('/about', (req, res) => {
-    res.status(200).json("Это простое приложение на Express.js")
+    res.status(200).send("Это простое приложение на Express.js")
 })
 
 router.all('*', (req, res) => {
-    res.status(404).json("Страница не найдена")
+    res.status(404).send("Страница не найдена")
 })
 
 export default router
